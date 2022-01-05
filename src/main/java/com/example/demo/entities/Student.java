7a593641d142e28652d814cae2a5ac8795afc806
karmarankartik <karmarankartik@gmail.com>
2022-01-05 03:08:24 -0600

    Update Student.java

--- src/main/java/com/example/demo/entities/Student.java
+++ src/main/java/com/example/demo/entities/Student.java
@@ -16,7 +16,7 @@ import javax.persistence.TemporalType;
 public class Student {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
 
